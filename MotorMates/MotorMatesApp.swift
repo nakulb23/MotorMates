7049a5e3bcac9004c7a4f217266d5b3772e53668
nakulb23 <154494941+nakulb23@users.noreply.github.com>
2025-08-18 23:20:43 -0700

    Initial Commit

---
+++ MotorMates/MotorMatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotorMatesApp.swift
+//  MotorMates
+//
+//  Created by Nakul Bhatnagar on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MotorMatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotorMatesTests/MotorMatesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MotorMatesTests.swift
+//  MotorMatesTests
+//
+//  Created by Nakul Bhatnagar on 8/18/25.
+//
+
+import Testing
+@testable import MotorMates
+
+struct MotorMatesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
